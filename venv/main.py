380bafbed94f0ddcc3d5b7fec53bcd51b4314f4a
acaceres81 <114486260+acaceres81@users.noreply.github.com>
2023-02-02 14:18:14 +0100

    02.02.2023

---
+++ venv/main.py
@@ -0,0 +1,19 @@
+print("Jokoak")
+print("---------------------------------")
+print("1.- Egunean behin")
+print("2.- Harri-Horri-Har")
+print("3.- Hangman")
+print("4.- Ez dut gahiago jolastu nahi.")
+print()
+aukera = int(input("zertan jokatu nahi duzu? "))
+match aukera:
+    case 1:
+        print("Asier")
+    case 2:
+        print("Asier")
+    case 3:
+        print("Asier")
+    case 4:
+        print("Goodbye, come again soon!")
+    case other:
+        print("that option does not exist.")
